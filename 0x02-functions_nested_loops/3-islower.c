a06c84bc6a892850def822897d939c1884acc746
TArOoO2 <mmansour9900@gmail.com>
2023-02-21 16:08:42 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10 - write the the lower-case alphabet on a line.
- *
- */
+* print_alphabet_x10 - write the the lower-case alphabet on a line.
+*
+*/
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
