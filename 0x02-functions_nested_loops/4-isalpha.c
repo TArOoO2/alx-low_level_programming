81cd96894ee51425285b641a03aed728aba8a79b
TArOoO2 <mmansour9900@gmail.com>
2023-02-21 16:19:19 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isalpha - check if the given character is lower case.
+* @c: character to test.
+*
+* Return: 1 (True) else 0 (False)
+*/
+int _islower(int c)
+{
+        if (c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z')
+        return (1);
+        else
+                return (0);
+}
